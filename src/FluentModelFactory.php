9deb2059d44da73b8f17ec82b7712e135331a644
Bob Horvers <bobhorvers@gmail.com>
2018-06-12 00:33:40 +0200

    Removed use mutators property from factory

--- src/FluentModelFactory.php
+++ src/FluentModelFactory.php
@@ -7,36 +7,10 @@ use Koetje\FluentModel\Contracts\FluentModelFactoryContract;
 class FluentModelFactory implements FluentModelFactoryContract
 {
     /**
-     * Use mutators when create a
-     * new fluent model instance.
-     * 
-     * @var bool
-     */
-    protected $useMutators = true;
-
-    /**
      * {@inheritdoc}
      */
-    public function make(array $attributes = [], $useMutators = null)
+    public function make(array $attributes = [], $useMutators = true)
     {
-        return FluentModel::make($attributes, $useMutators ?? $this->useMutators);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getUseMutators()
-    {
-        return $this->useMutators;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setUseMutators($value)
-    {
-        $this->useMutators = $value;
-
-        return $this;
+        return FluentModel::make($attributes, $useMutators);
     }
 }
