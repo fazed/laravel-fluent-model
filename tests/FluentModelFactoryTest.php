0b58b87603392ef68eb14686e5e4aa933081c1dd
Bob Horvers <bobhorvers@gmail.com>
2018-06-11 17:32:32 +0200

    Added factory test(s)

---
+++ tests/FluentModelFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Koetje\FluentModel\Tests;
+
+use Koetje\FluentModel\Contracts\FluentModelFactoryContract;
+
+class FluentModelFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_can_create_instance_wo_data()
+    {
+        $model = app(FluentModelFactoryContract::class)->make();
+
+        $this->assertEmpty($model->getAttributes());
+    }
+
+    /** @test */
+    public function it_can_create_instance_w_data()
+    {
+        $model = app(FluentModelFactoryContract::class)->make([
+            'some' => 'data'
+        ]);
+
+        $this->assertSame('data', $model->some);
+    }
+}
