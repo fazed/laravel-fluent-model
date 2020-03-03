f24f80bac80528fe381f6bf79d7ccca2c128ea03
fazed <thekoetjeable@gmail.com>
2020-03-03 23:36:42 +0100

    :white_check_mark: Fixed strict checks

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,22 +10,6 @@ abstract class TestCase extends Orchestra
     /**
      * {@inheritdoc}
      */
-    public function setUp()
-    {
-        parent::setUp();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        //
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     protected function getPackageProviders($app)
     {
         return [FluentModelServiceProvider::class];
